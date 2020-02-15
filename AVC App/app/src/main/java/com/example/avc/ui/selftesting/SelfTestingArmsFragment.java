5bcdd7472f2dbeee88b176f0c46be195d8d93d65
Cristian Simionescu <cristian@nexusmedia.ro>
2020-02-15 13:40:34 +0200

    Chaning arms

--- AVC App/app/src/main/java/com/example/avc/ui/selftesting/SelfTestingArmsFragment.java
+++ AVC App/app/src/main/java/com/example/avc/ui/selftesting/SelfTestingArmsFragment.java
@@ -56,75 +56,11 @@ public class SelfTestingArmsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         viewFinder = getView().findViewById(R.id.self_arms_view_finder);
-        System.out.println(viewFinder);
-
-        if (ContextCompat.checkSelfPermission(getContext(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.SEND_SMS,Manifest.permission.CALL_PHONE}, REQUEST_CODE_PERMISSIONS);
-        }
-        else{
-            //start Camera
-            viewFinder.post(this::startCamera);
-        }
-        viewFinder.addOnLayoutChangeListener((v, left, top, right, bottom, oldLeft, oldTop, oldRight, oldBottom) -> updateTransform());
 
         sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
         super.onViewCreated(view, savedInstanceState);
-    }
-
-    private void startCamera() {
-//        Rational rational = new Rational(1,1);
-        @SuppressLint("RestrictedApi") PreviewConfig previewConfig = new PreviewConfig.Builder().setLensFacing(CameraX.LensFacing.FRONT).build();
-        Preview preview = new Preview(previewConfig);
-
-        preview.setOnPreviewOutputUpdateListener(output -> {
-            ViewGroup parent = (ViewGroup) viewFinder.getParent();
-            parent.removeView(viewFinder);
-            parent.addView(viewFinder, 0);
-            SurfaceTexture surfaceTexture = output.getSurfaceTexture();
-            viewFinder.setSurfaceTexture(surfaceTexture);
-
-            updateTransform();
-        });
-
-        CameraX.bindToLifecycle( this, preview);
-    }
-
-    private void updateTransform() {
-        Matrix matrix = new Matrix();
-
-        //Find the center
-        float centerX = viewFinder.getWidth() / 2f;
-        float centerY = viewFinder.getHeight() / 2f;
-
-        //Get correct rotation
-        rotation = viewFinder.getDisplay().getRotation();
-
-        matrix.postRotate((float)-rotation, centerX, centerY);
-
-        float bufferRatio = 1.0f;
-
-        int scaledWidth;
-        int scaledHeight;
-        // Match longest sides together -- i.e. apply center-crop transformation
-        if (viewFinder.getWidth() > viewFinder.getHeight()) {
-            scaledHeight = viewFinder.getWidth();
-            scaledWidth = Math.round(viewFinder.getWidth() * bufferRatio);
-        } else {
-            scaledHeight = viewFinder.getHeight();
-            scaledWidth = Math.round(viewFinder.getHeight() * bufferRatio);
-        }
-
-        // Compute the relative scale value
-        float xScale = scaledWidth / (float)viewFinder.getWidth();
-        float yScale = scaledHeight / (float)viewFinder.getHeight();
-
-        // Scale input buffers to fill the view finder
-        matrix.preScale(xScale, yScale, centerX, centerY);
-
-
-        viewFinder.setTransform(matrix);
     }
 
     @Override
@@ -140,22 +76,10 @@ public class SelfTestingArmsFragment extends Fragment {
             if (ContextCompat.checkSelfPermission(getContext(),Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(getContext(), "Permisiunile pentru camera, SMS apel telefonic nu au fost acordate de utilizator.", Toast.LENGTH_SHORT).show();
                 getActivity().finish();
-            } else {
-                //start Camera
-                viewFinder.post(this::startCamera);
             }
         }
     }
 
-
-    private class SmilingSymmetryAnalyzer implements ImageAnalysis.Analyzer {
-        private long lastAnalyzedTimestamp = 0L;
-
-        @Override
-        public void analyze(ImageProxy image, int rotationDegrees) {
-
-        }
-    }
 
     @Override
     public void onStart() {
